0fce0b07d31ac1a8eeab1595c46a5199ce30f995
MD Abir Mahmud <abirmahmud5665@gmail.com>
2025-04-28 06:54:55 +0600

    done : enabled auth protection on get single order API

--- src/app/modules/orderManagement/order/order.routes.ts
+++ src/app/modules/orderManagement/order/order.routes.ts
@@ -24,10 +24,9 @@ router.get(
 
 router.get(
   "/admin/order-id/:id",
-  // authGuard({
-  //   requiredRoles: ["superAdmin", "admin", "staff"],
-  //   //   requiredPermission: "manage orders",
-  // }),
+  authGuard({
+    requiredRoles: ["superAdmin", "admin", "staff"],
+  }),
   OrderController.getOrderInfoByOrderIdAdmin
 );
 
