ff621bd66ee286c1a187e8503f1ab8cdfda43e00
MD ABIR MAHMUD <abirmahmud5665@gmail.com>
2026-01-22 18:39:00 +0600

    done : created homepage section apis

---
+++ src/app/modules/homepageSection/homepageSection.validation.ts
@@ -0,0 +1,22 @@
+import { z } from "zod";
+
+const createHomepageSection = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    subtitle: z.string({ required_error: "Subtitle is required!" }),
+    collectionId: z.string({ required_error: "Collection ID is required!" }),
+  }),
+});
+
+const updateHomepageSection = z.object({
+  body: z.object({
+    title: z.string().optional(),
+    subtitle: z.string().optional(),
+    collectionId: z.string().optional(),
+  }),
+});
+
+export const HomepageSectionValidation = {
+  createHomepageSection,
+  updateHomepageSection,
+};
