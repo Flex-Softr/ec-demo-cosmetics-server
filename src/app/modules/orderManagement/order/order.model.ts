1a8b1a0d6840c58c40ed5a85a0b291476b1749b5
mustaqimbd <mustaqimkhanbd@gmail.com>
2026-04-18 10:52:50 +0600

    Added missing the ref: Courier property

--- src/app/modules/orderManagement/order/order.model.ts
+++ src/app/modules/orderManagement/order/order.model.ts
@@ -69,6 +69,7 @@ const CourierDetailsSchema = new Schema<TCourierDetails>(
   {
     courierProvider: {
       type: mongoose.Schema.Types.ObjectId,
+      ref: "Courier",
     },
     trackingId: {
       type: String,
